9d6341ab6ff2943e18b4dee6f3781fcd5033a14c
maeva <maeganelys@gmail.com>
2022-01-27 09:13:38 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatefulWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  _MyappState createState() => _MyappState();
+}
+class _MyappState extends State<Myapp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+
+
+
+        ),
+      ),
+    );
+
+  }
