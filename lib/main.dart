7c2d1b75b2ad7dea6dd9e80107b77aab87ccf3b0
maeva <maeganelys@gmail.com>
2022-01-31 11:22:08 +0400

    add mail/mdp and state on connexion page

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ class Myapp extends StatefulWidget {
   @override
   _MyappState createState() => _MyappState();
 }
-class _MyappState extends State<Myapp> {
 
+class _MyappState extends State<Myapp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,35 +24,185 @@ class _MyappState extends State<Myapp> {
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.white.withOpacity(0),
-          leading: IconButton(icon: Icon(Icons.arrow_back,
-            color: Colors.black38,
-            size: 30,
-          ),
-              onPressed: () {
-                Navigator.pop(context);
-                },
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black38,
+              size: 30,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
         ),
         body: SingleChildScrollView(
-         child: Column(
-           children: [
-            DelayedAnimation(delay: 1000, child: Container(
-              height: 280,
-              child: Center(
-                child: CircleAvatar(radius: 100,
-                  backgroundImage: NetworkImage("https://64.media.tumblr.com/fa81b0bf867f4aa24b755f056cae1e0f/59eee7a1e9c2d24b-b3/s540x810/f87cdcbc413068fc05c32d7c5dee01fe194d5e57.png"),
-
-
+          child: Column(
+            children: [
+              DelayedAnimation(
+                delay: 1000,
+                child: Container(
+                  height: 280,
+                  child: Center(
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundImage: NetworkImage(
+                          "https://64.media.tumblr.com/fa81b0bf867f4aa24b755f056cae1e0f/59eee7a1e9c2d24b-b3/s540x810/f87cdcbc413068fc05c32d7c5dee01fe194d5e57.png"),
+                    ),
+                  ),
                 ),
               ),
-            ),
-            )
-             
-             
-           ],
-         ),
+              DelayedAnimation(
+                delay: 1000,
+                child: Container(
+                  height: 280,
+                  child: Center(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 40,
+                        horizontal: 30,
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Connexion",
+                            style: TextStyle(
+                              fontFamily: "Outfit",
+                              fontSize: 20,
+                              color: Colors.blueAccent,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Entrez vos informations de connexion",
+                            style: TextStyle(
+                              fontFamily: "Outfit",
+                              fontSize: 15,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w100,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 35),
+              // LoginForm(),
+              SizedBox(height: 125),
+              DelayedAnimation(
+                delay: 5500,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: StadiumBorder(),
+                    primary: Colors.redAccent,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 125,
+                      vertical: 13,
+                    ),
+                  ),
+                  child: Text(
+                    'CONFIRMER',
+                    style: TextStyle(
+                      fontFamily: "Outfit",
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Myapp(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 90),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 35),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: DelayedAnimation(
+                      delay: 6500,
+                      child: Text(
+                        "SKIP",
+                        style: TextStyle(
+                          fontFamily: "Outfit",
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var _obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 30,
+      ),
+      child: Column(
+        children: [
+          DelayedAnimation(
+            delay: 3500,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Adresse email',
+                labelStyle: TextStyle(
+                  color: Colors.grey[400],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          DelayedAnimation(
+            delay: 4500,
+            child: TextField(
+              obscureText: _obscureText,
+              decoration: InputDecoration(
+                labelStyle: TextStyle(color: Colors.grey[400]),
+                labelText: 'Mot de passe',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.visibility,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+}
